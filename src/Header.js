24e85560968dd1e55a82307c9427d3402b8358f0
Marysujitha <160560464+Marysujitha@users.noreply.github.com>
2025-09-08 20:25:13 +0530

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -7,10 +7,6 @@ function Header() {
 
       <nav>
         <a href="#">Home</a>
-        <a href="#">About</a>
-        <a href="#">AI</a>
-        <a href="#">Features</a>
-        <a href="#">Contact</a>
       </nav>
       <div className="login">
         <button onClick={() => changeButtonColor()}>Login</button>
